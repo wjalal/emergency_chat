5ded4b978a25e317e3afa02c83e719309dac766c
WJGalib <jalalwasif@gmail.com>
2025-02-17 21:04:28 +0600

    test credentials

--- client/src/lib/Login.svelte
+++ client/src/lib/Login.svelte
@@ -78,6 +78,7 @@
 			<Icon path={showPw ? mdiEyeOff : mdiEye} />
 		  </div>
 		</TextField>
+		<br>
 		<Button class="pa-4 mt-3 blue darken-3 grey-text text-lighten-5 rounded-xl align-self-center" 
 				on:click={onLogin}> <Icon path={mdiLogin} class="mr-3"/>  Login </Button>
 		<p class="grey-text text-caption">Test credentials — Usernames: test1, test2 — Password: ABCD1234</p>
